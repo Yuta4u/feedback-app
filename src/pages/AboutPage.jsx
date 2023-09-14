cf4037d781ba2d7c63dac462c309886667d6a9c2
Yuta4u <yudhistira4u@gmail.com>
2023-09-14 15:08:41 +0700

    update lagi

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -7,8 +7,8 @@ function AboutPage() {
       <div className="about">
         <h1>Tentang Project ini</h1>
         <p>
-          Project ini adalah project iseng untuk menerima feedback dari para
-          visitor sekalian.
+          Project ini adalah project iseng untuk belajar deploy, design,
+          sekaligus menerima feedback dari para visitor.
         </p>
         <p>
           <Link to="/">kembali ke halaman utama</Link>
